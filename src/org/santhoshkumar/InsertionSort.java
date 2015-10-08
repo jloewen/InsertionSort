539f786807ff7df6ffbc61143f7f5b65699d7e53
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-08 08:31:18 +0530

    Insertion sort implementation

---
+++ src/org/santhoshkumar/InsertionSort.java
@@ -0,0 +1,41 @@
+package org.santhoshkumar;
+
+public class InsertionSort {
+    int[] values;
+
+    public static void main(String[] args) {
+        int[] a = {9,8,7,6,5,3,2,4,1,0};
+        InsertionSort is = new InsertionSort();
+        is.values = a;
+        is.sort();
+        is.print();
+    }
+
+    // the usual logic is to store the ith element in temp variable and
+    // keep moving the items from j to j-1 until the item at j-1 is smaller.
+    // In the end move the ith item to jth position.
+    public void sort(){
+        for(int i =0; i < values.length; i ++){
+            for(int j=i; j > 0; j-- ){
+                if(values[j] < values[j-1]){
+                    swap(j, j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    private void swap(int from, int to){
+        int temp = values[from];
+        values[from] = values[to];
+        values[to] = temp;
+    }
+
+    public void print(){
+        for(int i = 0; i < values.length; i++){
+            System.out.print(values[i]+" ");
+        }
+        System.out.println();
+    }
+}
